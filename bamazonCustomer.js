dd2de7913bb36c0de4195782744fbc60b5bc8165
Lisa Smithen <smithenlisa@gmail.com>
2019-10-14 09:06:04 -0500

    Completed purchase prompt and purchase order function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -34,3 +34,52 @@ var displayProducts =function(){
 		purchasePrompt();
 	});
 }
+
+function purchasePrompt(){
+    inquirer.prompt([
+    {
+    name: "ID",
+    type: "input",
+    message: "Please enter the ID of the product they would like to buy.",
+    filter: Number
+},
+{
+
+name: "Quantity",
+type: "input",
+message: "How many units of the product they would like to buy?",
+filter: Number
+},
+
+]).then(function(answers){
+    var quantityNeeded = answers.Quantity;
+    var IDrequested = answers.ID;
+    purchaseOrder(IDrequested, quantityNeeded);
+});
+};
+
+function purchaseOrder(ID, amtNeeded){
+	connection.query('Select * FROM products WHERE item_id = ' + ID, function(err,res){
+		if(err){console.log(err)};
+		if(amtNeeded <= res[0].stock_quantity){
+			var totalCost = res[0].price * amtNeeded;
+			console.log("Good news your order is in stock!");
+			console.log("Your total cost for " + amtNeeded + " " +res[0].product_name + " is " + totalCost + " Thank you from ordering with us!");
+
+			connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + "WHERE item_id = " + ID);
+		} else{
+			console.log("Due to the high demand for our items we do not have sufficient stock in " + res[0].product_name + "to complete your order at this time.");
+		};
+		displayProducts();
+	});
+};
+
+displayProducts();
+
+
+
+
+
+
+
+}
