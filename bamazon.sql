6649aaccc4eff5e8ebc6b512dcd2da271a9675b3
Lisa Smithen <smithenlisa@gmail.com>
2019-10-13 22:04:09 -0500

    10 mock items were entered into table, including items for four departments, books, clothing, coaching, office

--- bamazon.sql
+++ bamazon.sql
@@ -15,4 +15,13 @@ Select * FROM products;
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quanity)
 VALUES (7000, "planners", "books", 35.00, 500),
-(
+(7500, "journals", "books", 25.00, 500),
+(8000, "hoodies", "clothing", 35.00, 300),
+(8500, "tshirts", "clothing", 30.00, 400),
+(3300, "workbooks", "coaching", 25.00, 1000),
+(3500, "entrepreneur courses", "coaching", 200.00, 1000),
+(3000, "coaching packages", 500.00, 200),
+(6300, "calendars", "office", 15.00, 300),
+(6000, "mousepads", "office", 7.00, 200),
+(6500, "bookmarks", "office", 5.00, 200);
+
